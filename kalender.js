40a91436d04f6d2e53d5143bc9195e37f8b9e188
André Kallehauge <andrekallehauge@gmail.com>
2013-10-19 23:55:10 +0200

    v2

--- kalender.js
+++ kalender.js
@@ -1,10 +1,13 @@
 function calendar(lang) {
 	// Defaults and variables
-		currentDay		= 	moment().format('DD');	 // Today's day.
-		currentMonth	= 	moment().format('MM');	 // Today's month.
-		currentYear 	= 	moment().format('YYYY'); // Today's year.
-		month 			=	currentMonth;	// Default month on load.
+	currentDay		= 	moment().format('DD');	 // Today's day.
+	currentMonth	= 	moment().format('MM');	 // Today's month.
+	currentYear 	= 	moment().format('YYYY'); // Today's year.
+	month 			=	currentMonth;	// Default month on load.
+	// Define default.
+	var monthChange = 0;
 
+	// Load on init
 	function staticContent(lang) {
 
 		function language(lang) {
@@ -31,98 +34,116 @@ function calendar(lang) {
 		tableHead();
 	}
 
-	// Define default.
-	var monthChange = 0;
-	// Run dynamic
-	function adjustCalendar(change) {
-		// Define default
+	// Will execute on init & prev + next buttons.
+	function dynamicContent(change, activeDay) {
+		// Define variables and set default.
 		change = change || 0;
-		// Determine how many months to add or subtract
-		monthChange += change;
-		//
-		newDate = moment().add('months', monthChange);
-		calendarMonth = newDate.format('MM');
-		calendarYear = newDate.format('YYYY');
-		calendarFull = newDate.format('MM-DD-YYYY');
+		activeDay = activeDay || null;
 
-		function appendMonth(month) {
-			monthText = moment(month).format('MMMM');
-			$('table caption').text(monthText);
-		}
+		// If the month have been changed
+		if (activeDay === null) {
+			// Determine how many months to add or subtract.
+			monthChange += change;
+			// New Date
+			newDate = moment().add('months', monthChange);
+			// Active month dates.
+			calendarMonth = newDate.format('MM');
+			calendarYear = newDate.format('YYYY');
+			calendarFull = newDate.format('MM-DD-YYYY');
 
-		// calculates amount of days in... bla bla
-		function pushFirstDay(month, year) {
-			theFirst = parseInt(moment(month+'-01-'+year,'MM-DD-YYYY').format('e')); // sunday = 0;
-			index = 1; // the days are 1-based, not 0.
-			return index - theFirst;
-		}
-
-		function appendMarkup(month, year, push) {
-			var appendStr;
-			x = 0;
-			daysInMonth = moment(calendarMonth+'-'+calendarYear, 'MM-YYYY').daysInMonth();
-			// Push the day & append each day in the month.
-			for (i = push; i <= daysInMonth; i++) {
-				x++;
-				if (x === 1) {
-					appendStr += '<tr><td>'+i+'</td>';
-				} else if(x === 7) {
-					appendStr += '<td>'+i+'</td></tr>';
-					x = 0;
-				} else {
-					appendStr += '<td>'+i+'</td>';
-				}
-			};
-			// Finish the week with blank days.
-			if (x != 0) {
-				for (i = 1; i <= (7-x); i++) {
-					appendStr += '<td></td>';
-				};
+			function appendMonth(month) {
+				monthText = moment(month).format('MMMM');
+				$('table caption').text(monthText);
 			}
-			appendStr += '</tr>';
 
-			// Remove existing table-rows (tr).
-			$('table tbody tr').remove();
+			function appendMarkup(month, year, push) {
+				// Define
+				var appendStr;
+				// Push
+				theFirst = parseInt(moment(month+'-01-'+year,'MM-DD-YYYY').format('e')); // monday = 0;
+				index = 1; // the days are 1-based, not 0.
+				push = index - theFirst;
+				// Days in month
+				daysInMonth = moment(calendarMonth+'-'+calendarYear, 'MM-YYYY').daysInMonth();
+				// Push the day & append each day in the month.
+				x = 0;
+				for (i = push; i <= daysInMonth; i++) {
+					x++;
+					if (x === 1) {
+						appendStr += '<tr><td>'+i+'</td>';
+					} else if(x === 7) {
+						appendStr += '<td>'+i+'</td></tr>';
+						x = 0;
+					} else {
+						appendStr += '<td>'+i+'</td>';
+					}
+				};
+				// Finish the week with blank days.
+				if (x != 0) {
+					for (i = 1; i <= (7-x); i++) {
+						appendStr += '<td></td>';
+					};
+				}
+				appendStr += '</tr>';
 
-			// Append new tablerows.
-			$('table tbody').append(appendStr);
+				// Remove existing table-rows (tr).
+				$('table tbody tr').remove();
+
+				// Append new tablerows.
+				$('table tbody').append(appendStr);
+			}
+
+			function styleMarkup() {
+				$('table tbody td').each(function() {
+					// Get date inside td's
+					calendarDate = $(this).text();
+					// If date is less than 1, make the day, blank.
+					if (calendarDate < 1) {
+						// Replace the html of "td" with null.
+						$(this).html('');
+					}
+					// If date is equal to today
+					if (calendarDate == currentDay) {
+						$(this).addClass('currentDay');
+					}
+				});
+			}
+
+			// Execute functions.
+			appendMonth( calendarMonth );
+			appendMarkup( calendarMonth, calendarYear );
+			styleMarkup();
 		}
 
-		function styleMarkup() {
-			$('table td').each(function() {
-				// Get date inside td's
-				calendarDate = $(this).text();
-				// If date is less than 1, make the day, blank.
-				if (calendarDate < 1) {
-					// Replace the html of "td" with null.
-					$(this).html('');
-				}
-				// If date is equal to today
-				if (calendarDate == currentDay) {
-					$(this).addClass('currentDay');
+		// If activeDay != null (a <td> have been clicked).
+		else {
+			$('tbody td').each(function() {
+				tdDate = $(this).text();
+				if (activeDay == tdDate) {
+					$(this).addClass('active');
+				} else {
+					$(this).removeClass('active');
 				}
 			});
 		}
-
-		// Execute functions.
-		// setMonth();
-		appendMonth( calendarMonth );
-		appendMarkup( calendarMonth, calendarYear, pushFirstDay(calendarMonth, calendarYear) );
-		styleMarkup();
-		// console.log(calendarFull); // Will parse: "MM-DD-YYYY"
 	}
 
 	// Init static content (ex: lang & table-head).
 	staticContent(lang);
 	// Init the calender
-	adjustCalendar();
+	dynamicContent();
 
 	// Prev / Next buttons
 	$('.prev').click(function() {
-		adjustCalendar(-1);
+		dynamicContent(-1);
 	});
 
 	$('.next').click(function() {
-		adjustCalendar(1);
+		dynamicContent(1);
+	});
+
+	$('table').delegate('tbody td', 'click', function() {
+		day = $(this).text();
+		dynamicContent(null, day);
 	});
 }
